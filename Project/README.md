7eef43d10e4c6008270ac1689fc628c67194c912
Daniel Marchena Parreira <daniel@Daniels-MacBook-Pro.local>
2018-11-01 00:25:52 -0400

    Push project code

---
+++ Project/README.md
@@ -0,0 +1,8 @@
+# Setting up input listener
+
+* MacOSX: `brew install portaudio`
+  * Necessary for capturing microphone input
+* Linux: `sudo apt-get install python-pyaudio python3-pyaudio`
+  * Necessary for capturing microphone input
+
+* `pip3 install -r requirements.txt`
---
+++ Project/speech_live_recognition.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+print(sr.Microphone.list_microphone_names())
+mic = sr.Microphone(device_index=0) # Internal mic
+
+with mic as source:
+    audio = r.listen(source)
+
+r.recognize_google(audio)
+
+# r = sr.Recognizer()
+#
+# harvard = sr.AudioFile('audio_files/jackhammer.wav')
+# with harvard as source:
+#     r.adjust_for_ambient_noise(source, duration=0.5)
+#     audio = r.record(source)
+#
+# print(r.recognize_google(audio, show_all=True))
